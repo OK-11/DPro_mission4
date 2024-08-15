1521ccfa8a6545c0562b0497362a9f84320a69c1
尾澤 公佑 <koushuke06@gmail.com>
2024-08-16 04:25:25 +0900

    課題提出

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,26 +2,31 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:orders).where(orders: {id:nil})
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop
+    @noOrderFoods = Food.left_outer_joins(:order_foods).where(order_foods: {food_id:nil})
+    @noOrderFoodsId = []
+    @noOrderFoods.each do |food|
+      @noOrderFoodsId << food.id
+    end
+    @shops = Shop.left_outer_joins(:foods).where(foods: {id:@noorderfoodsid})
   end
 
   def exercise3 
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address
+    @address = Address.joins(:orders).group("addresses.id").select("addresses.*,COUNT(orders.id) AS orders_count").order("orders_count DESC").first
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
+    @customer = Customer.joins(orders: :foods).group("customers.id").select("customers.*,SUM(foods.price) as foods_price_sum").order("foods_price_sum DESC").first
   end
 end
